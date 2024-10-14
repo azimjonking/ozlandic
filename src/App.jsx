ee556b8772936f743093689ce23bf963f13d3f97
azimjonking <azimjonjalilov1301@mail.ru>
2024-10-14 10:14:30 +0300

    ozlandic dev

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1 className="title">Ozlandic</h1>
-      <p className="subtitle">The website will launch soon.</p>
+      <h1>Ozlandic</h1>
     </div>
   );
 }
